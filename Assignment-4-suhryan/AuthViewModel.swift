209851a3bdd1446c3d98398e987a8bb3977b4037
ryan suh <ryansuh2002@gmail.com>
2024-11-14 23:30:43 -0500

    -created AuthViewModel and added login() function.

---
+++ Assignment-4-suhryan/AuthViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  AuthViewModel.swift
+//  Assignment-4-suhryan
+//
+//  Created by ryan suh on 11/14/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthViewModel: ObservableObject {
+    @Published var isSignedIn = false
+    @Published var errorMessage: String?
+
+    init() {
+        isSignedIn = Auth.auth().currentUser != nil
+    }
+
+    func login(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self?.errorMessage = error.localizedDescription
+                } else {
+                    self?.isSignedIn = true
+                }
+            }
+        }
+    }
+
+
+}
