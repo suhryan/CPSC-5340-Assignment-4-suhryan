ec83287cb4eae5adec13eb5aea5b9a37375a86b3
ryan suh <ryansuh2002@gmail.com>
2024-11-14 23:48:00 -0500

    -created LoginView.swift with email and password input elements

---
+++ Assignment-4-suhryan/LoginView.swift
@@ -0,0 +1,36 @@
+//
+//  LoginView.swift
+//  Assignment-4-suhryan
+//
+//  Created by ryan suh on 11/14/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+    @State private var email = ""
+    @State private var password = ""
+
+    var body: some View {
+        VStack {
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding()
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            if let errorMessage = authViewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+        }
+        .padding()
+    }
+}
+#Preview {
+    LoginView()
+}
