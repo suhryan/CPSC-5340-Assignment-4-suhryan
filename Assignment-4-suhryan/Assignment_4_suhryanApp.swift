79530b62e328bbe479a5bd955c46e5b3288919ea
ryan suh <ryansuh2002@gmail.com>
2024-11-14 22:00:55 -0500

    Initial Commit

---
+++ Assignment-4-suhryan/Assignment_4_suhryanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_4_suhryanApp.swift
+//  Assignment-4-suhryan
+//
+//  Created by ryan suh on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_4_suhryanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment-4-suhryan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment-4-suhryan
+//
+//  Created by ryan suh on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
